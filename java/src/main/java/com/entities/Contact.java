553e7ca6e014eda764970e103c015d3f7f4ebe57
dawid.paprocki90@gmail.com <dpaprocki@future-processing.com>
2019-08-05 09:30:44 +0200

    added contact classes

---
+++ java/src/main/java/com/entities/Contact.java
@@ -0,0 +1,15 @@
+package com.entities;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+public class ContactReason  extends BaseModel{
+
+}
